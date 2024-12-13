4e2871273f4a6206d6f0c3c649e6736349d4154e
BoneDeath <mhasannudin300@gmail.com>
2024-12-13 11:17:11 +0700

    asda

--- user-private-chat-auto-refresh.php
+++ user-private-chat-auto-refresh.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: User Private Chat Auto Refresh
  * Description: User Private Chat Auto Refresh is a WordPress plugin that enables logged-in users to communicate privately with each other through a chat system. The plugin displays a floating button that is only visible to logged-in users. Users can initiate a private conversation via a modal window, and the chat will automatically refresh to display the latest messages without the need to manually refresh the page. This plugin is designed to provide a more interactive and responsive communication experience within the site.
- * Version: 1.1.2
+ * Version: 1.1.3
  * Author: BoneDeath <a href="https://github.com/BoneDeath">Github</a>,<a href="https://www.linkedin.com/in/m-hasanuddin-webdev">LinkedIn</a>, Support me, <a href="https://buymeacoffee.com/mhasannudiz">Buy me a coffe ☕</a>
  * License: GPL2
  */
